148aef5040142f8caf642910a73a514f443ac29b
Olena Korenieva <olena.korenieva@gmail.com>
2023-06-27 19:51:03 +0200

    fixed index.js file with correct basename for router

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
   <Provider store={store}>
     <PersistGate loading={null} persistor={persistor}>
-      <BrowserRouter basename="/react-bc50-1-budget-boss">
+      <BrowserRouter basename="/budget-boss">
         <App />
       </BrowserRouter>
     </PersistGate>
